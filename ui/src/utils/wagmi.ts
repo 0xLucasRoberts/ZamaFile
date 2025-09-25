9cb32473e779fa99d16fc2f697bae5a7a37fa851
0xLucasRoberts <d137byfs03oo@hotmail.com>
2025-09-25 16:37:37 +0800

    remove old ui

--- ui/src/utils/wagmi.ts
+++
@@ -1,9 +0,0 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { sepolia } from 'wagmi/chains';
-
-export const config = getDefaultConfig({
-  appName: 'ZamaFile',
-  projectId: 'YOUR_PROJECT_ID', // Replace with your WalletConnect project ID
-  chains: [sepolia],
-  ssr: false,
-});
